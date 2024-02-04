c1f43a8689710b5ff3fbab57861f9f6b08fff165
sachini konara <140620408+SNKonara@users.noreply.github.com>
2024-02-04 07:39:58 +0530

    Add files via upload

---
+++ sum of integer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            if(num<=0)
+            {
+                Console.WriteLine("Error");
+                Console.ReadLine();
+            }
+            else
+            {
+                for(int i = 1; i <= num; i++)
+                {
+                    sum = sum + i;
+                }
+
+                Console.WriteLine("summation: " + sum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
