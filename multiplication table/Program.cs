e25dc14b99e1669a2528c328e77969088e49e563
sachini konara <140620408+SNKonara@users.noreply.github.com>
2024-02-04 07:42:04 +0530

    Add files via upload

---
+++ multiplication table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplication_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int multi = 1;
+
+            for(int i = 1; i < 13; i++)
+            {
+                multi = num * i;
+                Console.WriteLine(num + " *" + i + "= " + multi);
+               
+            }
+            Console.ReadLine() ;
+        } 
+
+    }
+}
