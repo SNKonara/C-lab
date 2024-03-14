9555482a017a31764168447eca77424a5820fcad
sachini konara <140620408+SNKonara@users.noreply.github.com>
2024-03-14 08:24:51 +0530

    Add files via upload

---
+++ Temperature Tracker/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Tracker
+{
+    public class TemperatureTracker
+    {
+        public int[] dailytemp;
+
+        public void temperatureTracker(int days)
+        {
+            dailytemp = new int[days];
+        }
+
+        public void settemp(int days,double temp)
+        {
+            for (days = 1; days <= dailytemp.Length; days++)
+            {
+
+                dailytemp[days] = (int)temp;
+            }
+        }
+
+        public void Displaytemperature()
+        {
+            Console.WriteLine("Weakly tempersture:");
+
+            for(int i = 0; i < dailytemp.Length; i++)
+            {
+                Console.WriteLine($"Temperature of day {i+1} is { dailytemp[i]}");
+            }
+        }
+
+
+
+            
+            
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int days = 7;
+            TemperatureTracker temp = new TemperatureTracker(days);
+            
+
+
+        }
+    }
+}
